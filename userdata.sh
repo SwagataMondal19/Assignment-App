bcfec6a89023798b0f937a1b82648701f92ade44
SwagataMondal19 <mondalswagata97@gmail.com>
2026-04-19 14:34:33 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
+# Update system
 yum update -y
+
+# Install packages
 yum install -y nodejs npm git amazon-cloudwatch-agent
 
+# App setup
 mkdir -p /var/www/app
 cd /var/www/app
 
@@ -11,12 +15,20 @@ git clone https://github.com/SwagataMondal19/Assignment-App.git .
 
 npm install
 
+# Fix ownership
+chown -R ec2-user:ec2-user /var/www/app
+
+# Run app as ec2-user (IMPORTANT)
+sudo -u ec2-user bash <<EOF
+cd /var/www/app
+
 npm install -g pm2
 pm2 start app.js --name app
 pm2 save
-
 pm2 startup systemd -u ec2-user --hp /home/ec2-user
+EOF
 
+# CloudWatch Agent config
 cat <<EOF > /opt/aws/amazon-cloudwatch-agent/bin/config.json
 {
   "metrics": {
@@ -36,6 +48,7 @@ cat <<EOF > /opt/aws/amazon-cloudwatch-agent/bin/config.json
 }
 EOF
 
+# Start CloudWatch Agent
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
 -a fetch-config \
 -m ec2 \
